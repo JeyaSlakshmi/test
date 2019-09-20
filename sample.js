e7a1168e8420cd5298858a6bbad4481c2f2763c6
jeyalakshmis <jeyalakshmis@teezle.com>
2019-09-20 14:10:17 +0530

    sample1 file again added

---
+++ sample.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jeyalakshmis on 20/9/19.
+ */
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
